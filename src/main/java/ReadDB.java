9f15d76c84c92f31fa78b05882082a869437c8a2
a.tkachuk <ast@loyverse.com>
2017-12-13 17:44:05 +0200

    сохранение топиков в файл

--- src/main/java/ReadDB.java
+++ src/main/java/ReadDB.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 
 public class ReadDB {
@@ -6,48 +10,76 @@ public class ReadDB {
     /**
      * Simple Java program to connect to MySQL database running on localhost and
      * running SELECT and INSERT query to retrieve and add data.
+     *
      * @author Javin Paul
      */
 
 
-        // JDBC URL, username and password of MySQL server
-        private static final String url = "jdbc:mysql://localhost:3306/lorg";
-        private static final String user = "root";
-        private static final String password = "root";
+    // JDBC URL, username and password of MySQL server
+    private static final String url = "jdbc:mysql://localhost:3306/lorg";
+    private static final String user = "root";
+    private static final String password = "root";
 
-        // JDBC variables for opening and managing connection
-        private static Connection con;
-        private static Statement stmt;
-        private static ResultSet rs;
+    // JDBC variables for opening and managing connection
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
 
-        public static void main(String args[]) {
-            String query = "select count(*) from qa_posts";
+    public static void main(String args[]) {
+        String queryTopic = "SELECT id,topic FROM qa_topics";
+        String queryPost = "SELECT * FROM qa_posts";
+        String queryUsers = "SELECT * From qa_users";
 
-            try {
-                // opening database connection to MySQL server
-                con = DriverManager.getConnection(url, user, password);
 
-                // getting Statement object to execute query
-                stmt = con.createStatement();
+        try {
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(url, user, password);
 
-                // executing SELECT query
-                rs = stmt.executeQuery(query);
+            // getting Statement object to execute query
+            stmt = con.createStatement();
 
-                while (rs.next()) {
-                    int count = rs.getInt(1);
-                    System.out.println("Total number of books in the table : " + count);
-                }
+            // executing SELECT query
+            rs = stmt.executeQuery(queryTopic);
 
-            } catch (SQLException sqlEx) {
-                sqlEx.printStackTrace();
-            } finally {
-                //close connection ,stmt and resultset here
-                try { con.close(); } catch(SQLException se) { /*can't do anything */ }
-                try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
-                try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+            while (rs.next()) {
+                String topic = rs.getString(2);
+                int id = rs.getInt(1);
+                System.out.println("index is  : " + id + " название: " + topic);
+                saveToFile(id, topic);
             }
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try {
+                con.close();
+            } catch (SQLException se) { /*can't do anything */ }
+            try {
+                stmt.close();
+            } catch (SQLException se) { /*can't do anything */ }
+            try {
+                rs.close();
+            } catch (SQLException se) { /*can't do anything */ }
         }
+    }
 
+    private static void saveToFile(int id, String topic) {
+        // определяем объект для каталога
+        File dir = new File("D:\\TempDir\\");
 
-
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        File fileName = new File("topic");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(dir +File.separator+ fileName.getName()+".csv", true))) {
+            // запись всей строки
+            String line = id + "," + topic + "\n";
+            writer.write(line);
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
