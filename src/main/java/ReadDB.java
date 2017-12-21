ec74c0d66a5ebb308cc92292f653876185fffe90
a.tkachuk <ast@loyverse.com>
2017-12-21 17:04:02 +0200

    загрузка вопросов с учетом условий

--- src/main/java/ReadDB.java
+++ src/main/java/ReadDB.java
@@ -52,10 +52,15 @@ public class ReadDB {
         String queryQu = "SELECT * FROM questions";
         String queryUsers = "SELECT * FROM for_user Where sellerid is not null";
         String insertTopics = "";
+        int slugInc=0;
+        String queryIdOwner = "SELECT fos_user.id " +
+                " FROM fos_user " +
+                " WHERE fos_user.userid = ";
 
         //List<List<String>> listOfTopic = readFromFile("D:\\Tempdir\\topic.csv", false);
-        //List<List<String>> listOfUsers = readFromFile("D:\\Tempdir\\users.csv", false);
+        List<List<String>> listOfUsers = readFromFile("D:\\Tempdir\\users.csv", false);
         List<List<String>> listOfQuestions = readFromFile("D:\\Tempdir\\questions.csv", true);
+        List<List<String>> listOfAnswers = readFromFile("D:\\Tempdir\\answers.csv", true);
         //listOfTopic.forEach(System.out::println);
         //listOfUsers.forEach(System.out::println);
 
@@ -65,44 +70,44 @@ public class ReadDB {
             con = DriverManager.getConnection(url2, userLORG2, passwordLORG2);
             System.out.println("Connected database successfully...");
             stmt = con.createStatement();
-/*            String insertSQLUsers = "INSERT INTO fos_user " +
-                    *//*1*//*      "(username," +
-                    *//*2*//*      "username_canonical," +
-                    *//*3*//*      "email," +
-                    *//*4*//*      "email_canonical," +
-                    *//*5*//*      "enabled," +
-                    *//*6*//*      "last_login, " +
-                    *//*7*//*      "userId," +
-                    *//*8*//*      "owner_id," +
-                    *//*9*//*      "localize," +
-                    *//*10*//*     "slug," +
-                    *//*11*//*     "segment," +
-                    *//*12*//*     "status," +
-                    *//*13*//*     "cropic," +
-                    *//*14*//*     "roles," +
-                    *//*15*//*     "is_owner," +
-                    *//*16*//*     "password)" +
+            String insertSQLUsers = "INSERT ignore INTO fos_user " +
+                    /*1*/      "(username," +
+                    /*2*/      "username_canonical," +
+                    /*3*/      "email," +
+                    /*4*/      "email_canonical," +
+                    /*5*/      "enabled," +
+                    /*6*/      "last_login, " +
+                    /*7*/      "userId," +
+                    /*8*/      "owner_id," +
+                    /*9*/      "localize," +
+                    /*10*/    "slug," +
+                    /*11*/    "segment," +
+                    /*12*/    "status," +
+                    /*13*/    "cropic," +
+                    /*14*/    "roles," +
+                    /*15*/    "is_owner," +
+                    /*16*/    "password)" +
 
                     "VALUES (" +
-                    *//*1  username*//*  "?," + //1
-                    *//*2  usm_cncl*//*  "?," + //2
-                    *//*3  email*//*     "?," + //3
-                    *//*4  eml_cnncl*//* "?," + //4
-                    *//*5  enabled*//*   "?," + //5
-                    *//*6  last_lgn*//*  "?," + //6
-                    *//*7  userid*//*    "?," + //7
-                    *//*8  owner_id*//*  "?," + //8
-                    *//*9  localize*//*  "'en'," +
-                    *//*10 slug*//*      "?," + //9
-                    *//*11 segment*//*   "'eng'," +
-                    *//*12 status*//*    "'Active'," +
-                    *//*13 cropic*//*    "'default.jpeg'," +
-                    *//*14 roles *//*    "'a:0:{}'," +
-                    *//*15 is Owner*//*  "?," + //10
-                    *//*16 password*//*  "?" +   //11
-                    " )";*/
+                    /*1  username*/  "?," + //1
+                    /*2  usm_cncl*/  "?," + //2
+                    /*3  email*/     "?," + //3
+                    /*4  eml_cnncl*/ "?," + //4
+                    /*5  enabled*/   "?," + //5
+                    /*6  last_lgn*/  "?," + //6
+                    /*7  userid*/    "?," + //7
+                    /*8  owner_id*/  "?," + //8
+                    /*9  localize*/  "'en'," +
+                    /*10 slug*/      "?," + //9
+                    /*11 segment*/   "'eng'," +
+                    /*12 status*/    "'Active'," +
+                    /*13 cropic*/    "'default.jpeg'," +
+                    /*14 roles */    "'a:0:{}'," +
+                    /*15 is Owner*/  "?," + //10
+                    /*16 password*/  "?" +   //11
+                    " )";
             Slugify slg = new Slugify();
-     /*       for (List<String> line : listOfUsers) {
+            for (List<String> line : listOfUsers) {
                 String slug = slg.slugify(line.get(5));// слаг для ю3ера
                 PreparedStatement statement = con.prepareStatement(insertSQLUsers, Statement.RETURN_GENERATED_KEYS);
                 String name_canonical = line.get(5).toLowerCase();
@@ -135,38 +140,155 @@ public class ReadDB {
                     System.out.println("Создание строки не удалось - " + email);
                 } else {
                     receiveIDinNewDB(line, statement);
-                }*/
-               /* if (affectedRow == 0) {
-                    throw new SQLException("Создание строки не удалось");
                 }
-                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                    if (generatedKeys.next()) {
-                        long id = generatedKeys.getLong(1);
-                        String idStr = String.valueOf(id);
-                        line.add(idStr);
-                    } else {
-                        throw new SQLException("Creating line failed, no ID obtained.");
-                    }
-                }*/
-                //System.out.println(line);
+            }
+            System.out.println("Finished to convert the users\n=== === === === ===\n=== === === === ===\n");
 
-            //}
 //вставка вопросов
-        for (List<String> line : listOfQuestions) {
-            String slug = slg.slugify(line.get(1));// слаг для топика
-            String insertSQLTopic = "INSERT INTO topics (topic, content, slug, cropic, segment)" +
-                    "VALUES (?,'short explanation about topic',?,'default_topic.jpeg', 'eng')";
-            //stmt.executeUpdate(insertSQLTopic);
-            PreparedStatement statement = con.prepareStatement(insertSQLTopic, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, line.get(1));
-            statement.setString(2, slug);
-            int affectedRow = statement.executeUpdate();
-               /* if (affectedRow == 0) {
-                    throw new SQLException("Создание строки не удалось");
-                }*/
-            receiveIDinNewDB(line, statement);
-            System.out.println(line);
-        }
+            System.out.println("Start to convert the questions");
+            for (List<String> line : listOfQuestions) {
+                String slug = slg.slugify(line.get(27)); // слаг для топика
+                int endIndexSlug = slug.length();
+                int slugIncLength = Integer.toString(slugInc).length();
+                if ((endIndexSlug+slugIncLength) > 30 ) {
+                    slug = slug.substring(0,30-slugIncLength) + slugInc;
+                } else {
+                    slug = slug + slugInc;
+                }
+                slugInc++;
+                String insertSQLQuestions = "INSERT ignore INTO questions (userid,title,content,slug,status,mailme,segment,created,updated)" +
+                        "VALUES (?,?,?,?,'active','1','en',?,?)";
+                if (line.get(11).contains("null")) {
+                    continue;
+                }
+                String userIdFromFOS_user = line.get(11);
+                String currentQueryIdOwner=queryIdOwner+userIdFromFOS_user;
+                rs = stmt.executeQuery(currentQueryIdOwner);
+                Boolean stateOfLine = rs.next();
+                String oldIdOwner;
+                if (stateOfLine) {
+                    oldIdOwner = rs.getString(1);
+                } else {
+                    continue;
+                }
+                PreparedStatement statement = con.prepareStatement(insertSQLQuestions, Statement.RETURN_GENERATED_KEYS);
+                String userid = oldIdOwner; //connectToFOS_UserID(line.get(11), listOfUsers);
+                String title = line.get(27);
+                String content = line.get(28);
+                String created = line.get(24);
+                if (created.contains("null")) {
+                    continue;
+                }
+                String updated = line.get(25);
+                if (updated.contains("null")) {
+                    updated = created;
+                }
+                if (userid.contains("null")) {
+                    continue;
+                }
+                statement.setString(1, userid);
+                statement.setString(2, title);
+                statement.setString(3, content);
+                statement.setString(4, slug);
+                statement.setString(5, created);
+                statement.setString(6, updated);
+                int affectedRow = statement.executeUpdate();
+                if (affectedRow == 0) {
+                    System.out.println("Создание строки не удалось - ");
+                } else {
+                    receiveIDinNewDB(line, statement);
+                }
+            }
+//вставка ответов
+          /*        "postid," + //      //0
+                    "type," +           //1
+                    "parentid," +       //2
+                    "categoryid," +     //3
+                    "catidpath1," +     //4
+                    "catidpath2," +     //5
+                    "catidpath3," +     //6
+                    "acount," +         //7
+                    "amaxvote," +       //8
+                    "selchildid," +     //9
+                    "closedbyid," +     //10
+                    "userid," +         //11
+                    "cookieid," +       //12
+                    "createip," +       //13
+                    "lastuserid," +     //14
+                    "lastip," +         //15
+                    "upvotes," +        //16
+                    "downvotes," +      //17
+                    "netvotes," +       //18
+                    "lastviewip," +     //19
+                    "views," +          //20
+                    "hotness," +        //21
+                    "flagcount," +      //22
+                    "format," +         //23
+                    "created," +        //24
+                    "updated," +        //25
+                    "updatetype," +     //26
+                    "title," +          //27
+                    "content," +        //28
+                    "name," +           //29
+                    "notify" +          //30*/
+            System.out.println("Start to convert the answers");
+            for (List<String> line : listOfAnswers) {
+                String slug = slg.slugify(line.get(28)); // слаг
+                int endIndexSlug = slug.length();
+                int slugIncLength = Integer.toString(slugInc).length();
+                if ((endIndexSlug + slugIncLength) > 30 ) {
+                    slug = slug.substring(0,30-slugIncLength) + slugInc;
+                } else {
+                    slug = slug + slugInc;
+                }
+                slugInc++;
+                if (line.get(11).contains("null")) {
+                    continue;
+                }
+                String insertSQLAnswers = "INSERT ignore INTO answers (qsid,userid,content,slug,mailme,created,updated)" +
+                        "VALUES (?,?,?,?,'0',?,?)";
+
+                String userIdFromFOS_user = line.get(11);
+                String currentQueryIdOwner=queryIdOwner+userIdFromFOS_user;
+                rs = stmt.executeQuery(currentQueryIdOwner);
+                Boolean stateOfLine = rs.next();
+                String oldIdOwner;
+                if (stateOfLine) {
+                    oldIdOwner = rs.getString(1);
+                } else {
+                    continue;
+                }
+                PreparedStatement statement = con.prepareStatement(insertSQLAnswers, Statement.RETURN_GENERATED_KEYS);
+
+                String userId = oldIdOwner;
+                String qsId = takeNewIdOfQuestion(line.get(0), listOfQuestions);
+
+                String content = line.get(28);
+                String created = line.get(24);
+                if (created.contains("null")) {
+                    continue;
+                }
+                String updated = line.get(25);
+                if (updated.contains("null")) {
+                    updated = created;
+                }
+                if (userId.contains("null")) {
+                    continue;
+                }
+                statement.setString(1, qsId);
+                statement.setString(2, userId);
+                statement.setString(3, content);
+                statement.setString(4, slug);
+                statement.setString(5, created);
+                statement.setString(6, updated);
+                int affectedRow = statement.executeUpdate();
+                if (affectedRow == 0) {
+                    System.out.println("Создание строки не удалось - ");
+                } else {
+                    receiveIDinNewDB(line, statement);
+                }
+                System.out.println(line);
+            }
 //вставка тем - топиков
 /*            for (List<String> line : listOfTopic) {
                 String slug = slg.slugify(line.get(1));// слаг для топика
@@ -212,21 +334,28 @@ public class ReadDB {
         }
     }
 
-    private static List<List<String>> readFromFileCoded(String fileName) throws IOException {
-        List<List<String>> listOfItems = Files.lines(Paths.get(fileName), StandardCharsets.UTF_8).map(ReadDB::parseLine).collect(toList());
-        return listOfItems;
+    private static String takeNewIdOfQuestion(String idOfQuestionOld, List<List<String>> listOfQuestions) {
+
+        for (List<String> line : listOfQuestions) {
+            if (idOfQuestionOld.equals(line.get(2))) { //
+                int max = line.size() - 1;
+                return line.get(max);
+            }
+        }
+        return "-1";
     }
 
-    private static void receiveIDinNewDB(List<String> line, PreparedStatement statement) throws SQLException {
+    private static List<String> receiveIDinNewDB(List<String> line, PreparedStatement statement) throws SQLException {
         try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
             if (generatedKeys.next()) {
                 long a = generatedKeys.getLong(1);
                 String aStr = String.valueOf(a);
                 line.add(aStr);
             } else {
-                throw new SQLException("Creating line failed, no ID obtained.");
+                System.out.println("Creating line failed, no ID obtained.");
             }
         }
+        return line;
     }
 
     public static String generateString(Random rng, String characters, int length) {
@@ -239,38 +368,26 @@ public class ReadDB {
 
     private static List<List<String>> readFromFile(String fileName, Boolean isEncoded) throws IOException {
         List<List<String>> listOfItems;
-        if (isEncoded) {
-            listOfItems = Files.lines(Paths.get(fileName), StandardCharsets.UTF_8).map(ReadDB::parseEncodedLine).collect(toList());
-        } else {
-            listOfItems = Files.lines(Paths.get(fileName), StandardCharsets.UTF_8).map(ReadDB::parseLine).collect(toList());
-        }
+        listOfItems = Files.lines(Paths.get(fileName), StandardCharsets.UTF_8).map(line -> parseLine(line, isEncoded)).collect(toList());
         return listOfItems;
     }
 
-    private static List<String> parseEncodedLine(String line)  {
+    private static List<String> parseLine(String line, Boolean isEncoded) {
         List list = new ArrayList();
         String[] subStr;
         String delimeter = ","; // Разделитель
         subStr = line.split(delimeter);
         // Вывод результата на экран
         for (String str : subStr) {
-            /*byte[] lineCoded = str.getBytes("UTF-8");
-            String encoded = Base64.getEncoder().encodeToString(str);*/
-            byte[] decoded = Base64.getDecoder().decode(str);
-            String encoded = Base64.getEncoder().encodeToString(decoded);
-            list.add(str);
-        }
-        return list;
-    }
+            if (isEncoded) {
+                byte[] lineCoded = str.getBytes();
+                byte[] decoded = Base64.getDecoder().decode(lineCoded);
+                String decodedStr = new String(decoded);
+                list.add(decodedStr);
+            } else {
+                list.add(str);
+            }
 
-    private static List<String> parseLine(String line) {
-        List list = new ArrayList();
-        String[] subStr;
-        String delimeter = ","; // Разделитель
-        subStr = line.split(delimeter);
-        // Вывод результата на экран
-        for (String str : subStr) {
-            list.add(str);
         }
         return list;
 
@@ -281,38 +398,38 @@ public class ReadDB {
     private static void saveDataFromLorg1() {
         String queryTopic = "SELECT id,topic FROM qa_topics";
         String queryPost = "SELECT " +
-                "postid," +
-                "type," +
-                "parentid," +
-                "categoryid," +
-                "catidpath1," +
-                "catidpath2," +
-                "catidpath3," +
-                "acount," +
-                "amaxvote," +
-                "selchildid," +
-                "closedbyid," +
-                "userid," +
-                "cookieid," +
-                "createip," +
-                "lastuserid," +
-                "lastip," +
-                "upvotes," +
-                "downvotes," +
-                "netvotes," +
-                "lastviewip," +
-                "views," +
-                "hotness," +
-                "flagcount," +
-                "format," +
-                "created," +
-                "updated," +
-                "updatetype," +
-                "title," +
-                "content," +
-                "name," +
-                "notify" +
-                " FROM qa_posts";
+                "postid," +         //0
+                "type," +           //1
+                "parentid," +       //2
+                "categoryid," +     //3
+                "catidpath1," +     //4
+                "catidpath2," +     //5
+                "catidpath3," +     //6
+                "acount," +         //7
+                "amaxvote," +       //8
+                "selchildid," +     //9
+                "closedbyid," +     //10
+                "userid," +         //11
+                "cookieid," +       //12
+                "createip," +       //13
+                "lastuserid," +     //14
+                "lastip," +         //15
+                "upvotes," +        //16
+                "downvotes," +      //17
+                "netvotes," +       //18
+                "lastviewip," +     //19
+                "views," +          //20
+                "hotness," +        //21
+                "flagcount," +      //22
+                "format," +         //23
+                "created," +        //24
+                "updated," +        //25
+                "updatetype," +     //26
+                "title," +          //27
+                "content," +        //28
+                "name," +           //29
+                "notify" +          //30
+                " FROM qa_posts WHERE userid IS NOT NULL";
 
         String queryUsers = "SELECT " +
                 "userid," +
